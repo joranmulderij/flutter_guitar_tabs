f482bae5b3b09dc7f7304a38f767801eeea652cc
joranmulderij <66296620+joranmulderij@users.noreply.github.com>
2023-09-18 11:43:46 +0200

    Make example a flutter project instead of single file

---
+++ example/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_guitar_tabs/flutter_guitar_tabs.dart';
+
+main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // Guitar
+          Center(
+            child: TabWidget(
+              name: 'Dm',
+              tabs: const ["x 0 0 2 3 1", "x 5 7 7 6 5", "10 12 12 10 10 10"],
+              size: 5,
+              color: Colors.black,
+              showStartFretNumber: true,
+            ),
+          ),
+          // Ukulele
+          Center(
+            child: TabWidget(
+              name: 'Dm',
+              tabs: const ["2 2 1 0"],
+              size: 5,
+              color: Colors.black,
+              showStartFretNumber: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
